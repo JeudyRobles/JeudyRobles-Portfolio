96b6a3af62313332d613e8e5b64cdb797be1056b
Jeudy Robles <jeudyjr@outlook.com>
2025-05-21 23:09:17 -0600

    Hero finished: responsive structure and layout optimization

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,8 +4,7 @@ export default function Button({text, variant}) {
     return (
     <>
     <a href="mailto:jeudyjr@outlook.com">
-      <button className={`Button button--${variant}`} class="Button">
-        <span class="Button__Shadow"></span>
+      <button className="Button">
         <span class="Button__Edge"></span>
         <div class="Button__Front">
           <span>{text} </span>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,26 +1,80 @@
 import './Hero.css'
 import Button from '../button/Button.jsx';
+import GitHubIcon from '../icons/Github.jsx';
+import FigmaIcon from '../icons/Figma.jsx';
+import LinkedInIcon from '../icons/LinkedIn.jsx';
+import WorldIcon from '../icons/World.jsx';
+
 
 export default function Hero() {
   return (
     <>
       {/* Hello world */}
-      <section class="Section Hero">
+      <section className="Section Hero">
 
-          <article class="Hero__Item Hero__Item--1">
-            <Button text="Open to work" variant="primary"/>
+          <article className="Hero__Item Hero__Item--1">
+            <Button text="Open to work"/>
             <h2>Web Developer</h2>
-            <p class="Hero__item--1---text">I have experience in creating customized websites, managing social media, and cybersecurity. I am currently seeking opportunities to contribute my skills to a dynamic and growing team.</p>
-            <Button text="Download CV" variant="secondary"/>
-            <Button text="LinkedIn" variant="outline"/>
+            <p className="Hero__item--1---text">I have experience in creating customized websites, managing social media, and cybersecurity. I am currently seeking opportunities to contribute my skills to a dynamic and growing team.</p>
+            <div className="Hero__Item--1__Buttons">
+            </div>
           </article>
           
-          <article class="Hero__Item Hero__Item--2">2</article>
-          <article class="Hero__Item Hero__Item--3">3</article>
-          <article class="Hero__Item Hero__Item--4">4</article>
-          <article class="Hero__Item Hero__Item--5">5</article>
-          <article class="Hero__Item Hero__Item--6">6</article>
-          <article class="Hero__Item Hero__Item--7">7</article>
+          <article className="Hero__Item Hero__Item--2">
+            <h3><span>Another </span>Platforms</h3>
+            <div className="Hero__Item--2__Icons">
+              <a href=""><GitHubIcon/></a>
+              <a href=""><FigmaIcon/></a>
+              <a href=""><LinkedInIcon/></a> 
+            </div>
+          </article>
+
+          <article className="Hero__Item Hero__Item--3">
+            <div className="Hero__Item--3__Div">
+              <h6>I am perpetually looking to evolve.</h6>
+              <h3>My Tech Stack</h3>
+            </div>
+            <div className="Hero__Item--3__Div Hero__Item--3__Div--TechStack">
+              <p>Javascript</p>
+              <p>React</p>
+              <p>Bootstrap</p>
+              <p>Tailwind</p>
+              <p>Wordpress</p>
+              <p>Python</p>
+            </div>
+          </article>
+
+          <article className="Hero__Item Hero__Item--4">
+            <h5>Professional Experience</h5>
+            <div className="">
+              <h6>Social Media Management</h6>
+              <p> -I managed the social networks of L'Patate and SICSA, creating content and strategies to increase interaction with the public.</p>
+            </div>
+            <div className="">
+              <h6>Social Media Management</h6>
+              <p> -I managed the social networks of L'Patate and SICSA, creating content and strategies to increase interaction with the public.</p>
+            </div>
+            <div className="">
+              <h6>Web Development & Freelance</h6>
+              <p> -Creation of customized websites with attractive design and focus on user experience.</p>
+            </div>
+            <div className="">
+              <h6>Customer Service & Communication</h6>
+              <p>   -Experience in face-to-face and digital customer service, ensuring a positive experience for users.</p>
+            </div>
+          </article>
+
+          <article className="Hero__Item Hero__Item--5"></article>
+
+          <article className="Hero__Item Hero__Item--6">
+            <WorldIcon/>
+            <p>Based in San Jose,</p>
+            <span>Costa Rica</span>
+          </article>
+
+          <article className="Hero__Item Hero__Item--7">
+            <a href="">@</a>
+          </article>
       </section>
     </>
   );
